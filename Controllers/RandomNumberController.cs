649d3c27e2508c4e6f8e3682538d2197c78777f1
Fr1senn <bombavlad1@gmail.com>
2023-10-05 17:50:38 +0300

    The controller that returns randomly generated numbers by GET method has been created

---
+++ Controllers/RandomNumberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareEngineering.Interfaces;
+
+namespace SoftwareEngineering.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumberController : ControllerBase
+    {
+        private readonly IRandomNumberGenerator _randomNumberGenerator;
+
+        public RandomNumberController(IRandomNumberGenerator randomNumberGenerator)
+        {
+            _randomNumberGenerator = randomNumberGenerator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRandomGeneratedNumbers()
+        {
+            return Ok(await _randomNumberGenerator.GenerateRandomNumbersAsync());
+        }
+    }
+}
