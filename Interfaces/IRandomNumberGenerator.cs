be50741456fbad30bba8ae0ed844648b19f90c53
Fr1senn <bombavlad1@gmail.com>
2023-10-05 17:44:52 +0300

    The service that creates randomly generated numbers has been created

---
+++ Interfaces/IRandomNumberGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftwareEngineering.Interfaces
+{
+    public interface IRandomNumberGenerator
+    {
+        public Task<List<int>> GenerateRandomNumbersAsync(int listLength = 100, int minAllowedValue = 1, int maxAllowedValue = 6);
+    }
+}
---
+++ Services/RandomNumberGenerator.cs
@@ -0,0 +1,26 @@
+﻿using SoftwareEngineering.Interfaces;
+
+namespace SoftwareEngineering.Services
+{
+    public class RandomNumberGenerator : IRandomNumberGenerator
+    {
+        public Task<List<int>> GenerateRandomNumbersAsync(int listLength = 100, int minAllowedValue = 1, int maxAllowedValue = 6)
+        {
+            List<int> numbers = new List<int>();
+            Random random = new Random();
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < listLength; i++)
+                {
+                    lock (random)
+                    {
+                        numbers.Add(random.Next(minAllowedValue, maxAllowedValue + 1));
+                    }
+                }
+            });
+
+            return Task.FromResult(numbers);
+        }
+    }
+}
