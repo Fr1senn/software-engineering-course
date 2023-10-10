74490ea7f45b771d4ff8d1d8d589b4f2906f7511
Fr1senn <bombavlad1@gmail.com>
2023-10-10 13:38:14 +0300

    The DB context service has been injected

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using SoftwareEngineering.Interfaces;
 using SoftwareEngineering.Interfaces.Services;
+using SoftwareEngineering.Models;
 using SoftwareEngineering.Repositories;
 using SoftwareEngineering.Services;
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LibraryContext>();
 
 builder.Services.AddCors(options =>
 {
