02c6bf4bedf34ef65912cdf8bc872365cfb10676
Fr1senn <bombavlad1@gmail.com>
2023-10-10 13:32:55 +0300

    The controller for the work with books has been created

---
+++ Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareEngineering.Interfaces;
+
+namespace SoftwareEngineering.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooksAsync()
+        {
+            return Ok(await _bookRepository.GetBooksAsync());
+        }
+    }
+}
