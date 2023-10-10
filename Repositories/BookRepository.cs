0eef59e3aa94eb59672885320425f2f25e7a0d84
Fr1senn <bombavlad1@gmail.com>
2023-10-10 13:32:04 +0300

    The implementation for the IBookRepository has been added

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftwareEngineering.Interfaces;
+using SoftwareEngineering.Models;
+
+namespace SoftwareEngineering.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BookRepository(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+        public async Task<IEnumerable<BooksAuthor>> GetBooksAsync()
+        {
+            List<BooksAuthor> books = await _libraryContext.BooksAuthors
+                .Include(bA => bA.Book)
+                .Include(bA => bA.Author)
+                .AsNoTracking()
+                .ToListAsync();
+            return books;
+        }
+    }
+}
