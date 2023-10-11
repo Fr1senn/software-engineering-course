9012d251267a81aef626eadc27baeae1558914cc
Fr1senn <bombavlad1@gmail.com>
2023-10-11 16:04:47 +0300

    The method that creates a new book has been implemented

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SoftwareEngineering.Interfaces;
 using SoftwareEngineering.Models;
+using SoftwareEngineering.Models.DTOs;
 
 namespace SoftwareEngineering.Repositories
 {
@@ -13,7 +14,45 @@ namespace SoftwareEngineering.Repositories
             _libraryContext = libraryContext;
         }
 
-        public async Task<IEnumerable<BooksAuthor>> GetBooksAsync()
+        public async Task CreateBookAsync(BookDTO bookDTO)
+        {
+            if (bookDTO is null)
+                throw new NullReferenceException("Invalid Data");
+
+            Book book = new Book
+            {
+                Title = bookDTO.Title,
+                PublicationDate = bookDTO.PublicationDate,
+                Orders = new List<Order>(),
+                BooksAuthors = new List<BooksAuthor>()
+            };
+
+            if (bookDTO.Authors != null)
+            {
+                foreach (var authorDTO in bookDTO.Authors)
+                {
+                    var existingAuthor = await _libraryContext.Authors.SingleOrDefaultAsync(a => a.FullName == authorDTO.FullName);
+
+                    if (existingAuthor != null)
+                    {
+                        var booksAuthor = new BooksAuthor
+                        {
+                            AuthorId = existingAuthor.Id,
+                            Book = book
+                        };
+
+                        book.BooksAuthors.Add(booksAuthor);
+                    }
+                    else
+                    {
+                        throw new NullReferenceException("Author not found");
+                    }
+                }
+            }
+
+            _libraryContext.Books.Add(book);
+            await _libraryContext.SaveChangesAsync();
+        }
 
         public async Task DeleteBookAsync(int id)
         {
