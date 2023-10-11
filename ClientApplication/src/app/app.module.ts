a81874c45ff80545a853c642cab64527317fcbd0
Fr1senn <bombavlad1@gmail.com>
2023-10-11 16:15:58 +0300

    The module has been injected

--- ClientApplication/src/app/app.module.ts
+++ ClientApplication/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RandomNumberComponent } from './components/random-number/random-number.component';
 import { NgChartsModule } from 'ng2-charts';
+import { LibraryModule } from './modules/library/library.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NgChartsModule } from 'ng2-charts';
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
-    NgChartsModule
+    NgChartsModule,
+    LibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
