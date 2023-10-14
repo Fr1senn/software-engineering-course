4c05eb629d54b3557ad8fe75319708c717960740
Fr1senn <bombavlad1@gmail.com>
2023-10-14 18:35:59 +0300

    The controller with corresponding repository methods has been implemented

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareEngineering.Interfaces;
+using SoftwareEngineering.Models.DTOs;
+using SoftwareEngineering.Repositories;
+
+namespace SoftwareEngineering.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrdersAsync()
+        {
+            return Ok(await _orderRepository.GetOrdersAsync());
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOrderAsync([FromQuery] int orderId)
+        {
+            try
+            {
+                await _orderRepository.DeleteOrderAsync(orderId);
+                return Ok();
+            }
+            catch (NullReferenceException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderAsync([FromBody] OrderDTO orderDTO)
+        {
+            try
+            {
+                await _orderRepository.CreateOrderAsync(orderDTO);
+                return Ok();
+            }
+            catch(NullReferenceException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
