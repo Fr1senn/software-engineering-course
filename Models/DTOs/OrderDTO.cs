d7a10ca0446f725d081b433aa982d884e3f23114
Fr1senn <bombavlad1@gmail.com>
2023-10-14 18:34:48 +0300

    The DTO model has been created

---
+++ Models/DTOs/OrderDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftwareEngineering.Models.DTOs
+{
+    public class OrderDTO
+    {
+        public int? Id { get; set; }
+
+        public DateOnly OrderDate { get; set; }
+
+        public DateOnly? RefundDate { get; set; }
+
+        public Book Book { get; set; }
+
+        public Reader Reader { get; set; }
+    }
+}
